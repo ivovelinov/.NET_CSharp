ef132a1f122437cd515f87e5036ced7770353adf
ivovelinov <ivovelinov@gmail.com>
2023-09-04 23:55:13 -0700

    Short Backgroundworker example

---
+++ BackgroiundWorkerExample1/BackgroiundWorkerExample1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroiundWorkerExample1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.WorkerReportsProgress = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerReportsProgress == true)
+            {
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                else
+                {
+                    System.Threading.Thread.Sleep(500);
+                    worker.ReportProgress(i * 10);
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label1.Text = e.ProgressPercentage.ToString() + "%";
+            progressBar1.Value = e.ProgressPercentage;
+            
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                label1.Text = "Background worker was canceled";
+                progressBar1.Value = 0;
+            }
+            else if (e.Error != null)
+            {
+                label1.Text = "Error: " + e.Error.ToString();
+                progressBar1.Value = 0;
+            }
+            else
+            {
+                label1.Text = "Background work compleeted";
+                progressBar1.Value = 100;
+            }
+        }
+    }
+}
